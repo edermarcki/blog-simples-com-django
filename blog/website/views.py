129de39f62a5f095e5294a730c73b64c35a7a3f2
Eder Marcki Jr <edermarcki@live.com>
2021-07-19 16:59:42 -0300

    Exibindo os posts do banco de dados no template

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
 def hello_blog(request):
     lista = ['Django', 'Python', 'Git', 'Banco de Dados',
     'HTML', 'Linux', 'UWSGI', 'Nginx', 'Systemctl']
-    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+    list_posts = Post.objects.all()
+    
+    data = {'name': 'Curso de Django 3',
+    'lista_tecnologias': lista,
+    'posts': list_posts}
+    
+    
     return render(request, 'index.html', data)
