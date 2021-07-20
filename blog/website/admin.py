d1b7b20f081e1ccb60f6650f89625973e5f7fe91
Eder Marcki Jr <edermarcki@live.com>
2021-07-20 13:54:02 -0300

    Criação das categorias dos posts e personalização do Django admin

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_title', 'categories']
+    search_fields = ['title', 'sub_title', 'categories']
+
+    def get_queryset(self, request):
+        return Post.objects.filter(deleted=False)
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
