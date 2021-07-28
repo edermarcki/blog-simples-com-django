7f39e74b9916e0c3815c234dfc1ad0a0cf4ce89f
Eder Marcki Jr <edermarcki@live.com>
2021-07-28 11:36:59 -0300

    Criação do formulário de contato

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Contact
 
 
 # Create your views here.
@@ -19,3 +19,14 @@ def hello_blog(request):
 def post_detail(request, id):
     post = Post.objects.get(id=id)
     return render(request, 'post_detail.html', {'post': post})
+
+
+def save_form(request):
+    name = request.POST['name']
+    Contact.objects.create(
+        name= name,
+        email= request.POST['email'],
+        message= request.POST['message']
+    )
+    return render(request, 'contact_sucess.html',
+    {'name_contact': name})
